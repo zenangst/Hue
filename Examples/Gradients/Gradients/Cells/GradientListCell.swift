cae772a865e7eb0f156c464bc8deeaf7c3defb14
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-30 13:28:00 +0100

    Add selected view to cell

--- Examples/Gradients/Gradients/Cells/GradientListCell.swift
+++ Examples/Gradients/Gradients/Cells/GradientListCell.swift
@@ -4,11 +4,18 @@ import Hue
 
 class GradientListCell: ListSpotCell {
 
+  lazy var selectedView: UIView = {
+    let view = UIView()
+    view.backgroundColor = Color.cellSelectedColor
+    return view
+  }()
+
   override func configure(inout item: ListItem) {
-    backgroundColor = UIColor.clearColor()
-    textLabel?.font = Font.cell
-    textLabel?.text = item.title
     textLabel?.textColor = UIColor.hex("#fff").alpha(0.8)
+    textLabel?.text = item.title
+    textLabel?.font = Font.cell
+    selectedBackgroundView = selectedView
+    backgroundColor = UIColor.clearColor()
 
     item.size = CGSize(width: 64, height: 64)
   }
