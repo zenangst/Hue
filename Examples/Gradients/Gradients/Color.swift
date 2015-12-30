a3c4a89dbae19efe51d50d6d3e254ca1058346bf
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-30 13:51:56 +0100

    Remove header comments

--- Examples/Gradients/Gradients/Color.swift
+++ Examples/Gradients/Gradients/Color.swift
@@ -1,11 +1,3 @@
-//
-//  Color.swift
-//  Cheep
-//
-//  Created by Christoffer Winterkvist on 02/08/15.
-//  Copyright (c) 2015 zenangst. All rights reserved.
-//
-
 import UIKit
 
 struct Color {
--- Examples/Gradients/Gradients/Font.swift
+++ Examples/Gradients/Gradients/Font.swift
@@ -1,11 +1,3 @@
-//
-//  Font.swift
-//  Cheep
-//
-//  Created by Christoffer Winterkvist on 02/08/15.
-//  Copyright (c) 2015 zenangst. All rights reserved.
-//
-
 import UIKit
 
 struct Font {
