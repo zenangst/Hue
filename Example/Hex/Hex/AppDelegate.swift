f05542b945e13466c930d199bf2f7754e4667ed8
Khoa Pham <onmyway133@gmail.com>
2017-06-19 13:27:19 +0200

    Update Hex demo

--- Example/Hex/Hex/AppDelegate.swift
+++ Example/Hex/Hex/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Spots
-import Brick
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -12,51 +11,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     window = UIWindow(frame: UIScreen.main.bounds)
 
-    CarouselSpot.register(defaultView: GridHexCell.self)
-    CarouselSpot.configure = { collectionView, _ in
-      collectionView.backgroundColor = UIColor.black
+    Configuration.registerDefault(view: GridHexCell.self)
+    Component.configure = {
+      $0.collectionView?.backgroundColor = UIColor.black
     }
 
-    let controller = SpotsController(spots: [
-      CarouselSpot(Component(title: "CMYK", span: 3, items:
-        [
-          Item(title: "#00FFFF"),
-          Item(title: "#FF00FF"),
-          Item(title: "#FFFF00"),
-          Item(title: "#000000")
-        ]
-        ), top: 5, left: 15, bottom: 5, right: 15, itemSpacing: 15),
-      CarouselSpot(Component(title: "Facebook", span: 3, items:
-        [
-          Item(title: "#3b5998"),
-          Item(title: "#8b9dc3"),
-          Item(title: "#dfe3ee"),
-          Item(title: "#f7f7f7"),
-          Item(title: "#ffffff")
-        ]
-        ), top: 5, left: 15, bottom: 5, right: 15, itemSpacing: 15),
-      CarouselSpot(Component(title: "Rainbow Dash", span: 3, items:
-        [
-          Item(title: "#ee4035"),
-          Item(title: "#f37736"),
-          Item(title: "#fdf498"),
-          Item(title: "#7bc043"),
-          Item(title: "#0392cf")
-        ]
-        ), top: 5, left: 15, bottom: 5, right: 15, itemSpacing: 15),
-      CarouselSpot(Component(title: "Beach", span: 3, items:
-        [
-          Item(title: "#96ceb4"),
-          Item(title: "#ffeead"),
-          Item(title: "#ff6f69"),
-          Item(title: "#ffcc5c"),
-          Item(title: "#88d8b0")
-        ]
-        ), top: 5, left: 15, bottom: 5, right: 15, itemSpacing: 15),
-      ]
-    )
+    let layout = Layout(span: 3,
+                        inset: Inset(top: 5, left: 15, bottom: 15, right: 15))
 
-    controller.spotsScrollView.contentInset.top = 15
+    let componentModels: [ComponentModel] = [
+      ComponentModel(kind: .carousel, layout: layout, items: [
+        Item(title: "#00FFFF"),
+        Item(title: "#FF00FF"),
+        Item(title: "#FFFF00"),
+        Item(title: "#000000")
+      ]),
+      ComponentModel(kind: .carousel, layout: layout, items: [
+        Item(title: "#3b5998"),
+        Item(title: "#8b9dc3"),
+        Item(title: "#dfe3ee"),
+        Item(title: "#f7f7f7"),
+        Item(title: "#ffffff")
+      ]),
+      ComponentModel(kind: .carousel, layout: layout, items: [
+        Item(title: "#ee4035"),
+        Item(title: "#f37736"),
+        Item(title: "#fdf498"),
+        Item(title: "#7bc043"),
+        Item(title: "#0392cf")
+      ]),
+      ComponentModel(kind: .carousel, layout: layout, items: [
+        Item(title: "#96ceb4"),
+        Item(title: "#ffeead"),
+        Item(title: "#ff6f69"),
+        Item(title: "#ffcc5c"),
+        Item(title: "#88d8b0")
+      ]),
+    ]
+
+    let components = componentModels.map { Component(model: $0) }
+    let controller = SpotsController(components: components)
+
+    controller.scrollView.contentInset.top = 15
 
     window?.rootViewController = controller
     window?.makeKeyAndVisible()
